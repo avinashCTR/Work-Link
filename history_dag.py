55628805cb5beb6df2ae1fcd94758e0e87e1a13b
avinashCTR <avinash.reddy@couture.ai>
2025-03-25 18:33:37 +0530

    asdasd

---
+++ history_dag.py
@@ -0,0 +1,15 @@
+ColdCacheQueries = CoutureSparkOperator(
+    task_id='ColdCacheQueries',
+    method_id='GetColdCacheQueries',
+    class_path=classPath,
+    code_artifact=code_artifact,
+    method_args_dict={"search_term_column": "search_term",
+                       "freq_colunm": "total_searches",
+                       "topk": 100000},
+    input_base_dir_path="",
+    output_base_dir_path="",
+    input_filenames_dict={"search_term_data":"/data1/searchengine/rawdata/historydata/jiomart/dump/query_level_data"},
+    output_filenames_dict={"cold_cache_queries": "ColdCacheQueries"},
+    dag=Dag,
+    description=''
+)

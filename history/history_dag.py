82aecc1369684722ed043ff531b8e48c51bb1ccb
avinashCTR <avinash.reddy@couture.ai>
2025-03-31 16:47:17 +0530

    asdasd

--- history/history_dag.py
+++ history/history_dag.py
@@ -28,3 +28,24 @@ ParseCleanAndNormaliseHistoryData = CoutureSparkOperator(
     dag=Dag,
     description=''
 )
+
+PreProcessHistory = CoutureSparkOperator(
+    task_id='PreProcessHistory',
+    application=classPath,
+    code_artifact=code_artifact,
+    method_args_dict={
+        "search_term_column":"search_term",
+        "freq_column":"product_unique_list_view",
+        "counts_coulumn":"product_unique_list_clicks",
+        "product_id_column":"sku"
+    },
+    input_base_dir_path="",
+    output_base_dir_path="",
+    input_filenames_dict={"query_product_interactions":"/data1/archive/",
+    "query_position_data":"/data1/archive/"},
+    output_filenames_dict={"history_queries_data_archive": "ColdCacheQueries",
+    "history_queries_data":"asdasdasd"
+    },
+    dag=Dag,
+    description=''
+)
